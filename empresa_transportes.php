274166ca7c3576e00c8665a50cf314bdae265a95
anibalyucra20 <anibalyucrac@gmail.com>
2023-11-13 18:07:31 -0500

    update conexion

--- empresa_transportes.php
+++ empresa_transportes.php
@@ -1,6 +1,6 @@
 <?php
-include("../include/conexion.php");
-include("../include/busquedas.php");
+include("include/conexion.php");
+include("include/busquedas.php");
 ?>
 <!DOCTYPE html>
 <html lang="es">
